e8459928cd5c335c90e7308eda048bc28803151e
DESKTOP-9U04SLI\Sale <aleksandarjovisic991@gmail.com>
2019-04-29 20:04:20 +0200

    First commit

---
+++ src/main/java/com/example/SpringDataJpa/controller/UserRestController.java
@@ -0,0 +1,42 @@
+package com.example.SpringDataJpa.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.SpringDataJpa.model.User;
+import com.example.SpringDataJpa.repository.UserRepository;
+
+
+@RestController
+@RequestMapping(value="/rest/users")
+public class UserRestController {
+
+	@Autowired
+    UserRepository userRepository;
+	
+    @GetMapping("/all")
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    
+    @GetMapping("/update/{id}/{username}/{password}")
+    public User update(@PathVariable("id") final Integer id, @PathVariable("username")
+                         final String username, @PathVariable("password") final String password) {
+
+
+        User user = new User();
+        user.setId(id);
+        user.setUsername(username);
+        user.setPassword(password);
+     
+        
+        return userRepository.save(user);
+    }
+	
+}
---
+++ src/main/java/com/example/SpringDataJpa/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.SpringDataJpa.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.SpringDataJpa.model.User;
+
+
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    List<User> findByUsername(String username);
+
+
+}
